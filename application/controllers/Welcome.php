4edf0ecc7e7725a32e27c2d34049cd9ea65d8f8b
Anthony <13015521+darknoon29@users.noreply.github.com>
2015-08-20 20:07:47 +0200

    Graphiques

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,11 +25,38 @@ class Welcome extends CI_Controller {
         $ogspy_users = $this->ogspystats_model->get_total_users();
         $ogspy_servers = $this->ogspystats_model->get_total_servers();
 
+        $php_versions = $this->ogspystats_model->get_php_versions();
+        $php_versions_hc = $this->convert_to_highcharts($php_versions);
+
+        $ogspy_versions = $this->ogspystats_model->get_ogspy_versions();
+        $ogspy_versions_hc = $this->convert_to_highcharts($ogspy_versions);
+
+        $ogspy_unis = $this->ogspystats_model->get_uni_number();
+        $ogspy_unis_hc = $this->convert_to_highcharts($ogspy_unis);
+
+        $ogspy_pays = $this->ogspystats_model->get_uni_pays();
+        $ogspy_pays_hc = $this->convert_to_highcharts($ogspy_pays);
+
         $data = array(
             'nb_users' => $ogspy_users,
-            'nb_servers' => $ogspy_servers
+            'nb_servers' => $ogspy_servers,
+            'php_versions' => $php_versions_hc,
+            'ogspy_versions' => $ogspy_versions_hc,
+            'ogspy_uni' => $ogspy_unis_hc,
+            'ogspy_pays' => $ogspy_pays_hc
         );
 
         $this->load->view('welcome_message', $data);
 	}
+
+    private function convert_to_highcharts($data_to_convert){
+        $temp = "";
+        foreach($data_to_convert as $key => $value){
+
+             $temp[]= "['" . $key . "'," . $value . " ]";
+        }
+        // format hightchart
+        $hc_table = implode(" , ", $temp);
+        return($hc_table);
+    }
 }
