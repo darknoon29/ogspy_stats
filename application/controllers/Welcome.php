b5ee15d9637426f0180384307f5924f6cfdcec21
Anthony <13015521+darknoon29@users.noreply.github.com>
2015-10-10 18:01:56 +0200

    Ajout des statistiques Modules

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -26,16 +26,21 @@ class Welcome extends CI_Controller {
         $ogspy_servers = $this->ogspystats_model->get_total_servers();
 
         $php_versions = $this->ogspystats_model->get_php_versions();
-        $php_versions_hc = $this->convert_to_highcharts($php_versions);
+        $php_versions_hc = $this->convert_to_highcharts_pie($php_versions);
 
         $ogspy_versions = $this->ogspystats_model->get_ogspy_versions();
-        $ogspy_versions_hc = $this->convert_to_highcharts($ogspy_versions);
+        $ogspy_versions_hc = $this->convert_to_highcharts_pie($ogspy_versions);
 
         $ogspy_unis = $this->ogspystats_model->get_uni_number();
-        $ogspy_unis_hc = $this->convert_to_highcharts($ogspy_unis);
+        $ogspy_unis_hc = $this->convert_to_highcharts_pie($ogspy_unis);
 
         $ogspy_pays = $this->ogspystats_model->get_uni_pays();
-        $ogspy_pays_hc = $this->convert_to_highcharts($ogspy_pays);
+        $ogspy_pays_hc = $this->convert_to_highcharts_pie($ogspy_pays);
+
+        /* Modules */
+
+        $ogspy_mods_stats = $this->ogspy_mods_stats_model->get_mod_rank();
+        $ogspy_mods_stats_hc = $this->convert_to_highcharts_bar($ogspy_mods_stats);
 
         $data = array(
             'nb_users' => $ogspy_users,
@@ -43,13 +48,15 @@ class Welcome extends CI_Controller {
             'php_versions' => $php_versions_hc,
             'ogspy_versions' => $ogspy_versions_hc,
             'ogspy_uni' => $ogspy_unis_hc,
-            'ogspy_pays' => $ogspy_pays_hc
+            'ogspy_pays' => $ogspy_pays_hc,
+            'ogspy_mod_rank_name' => $ogspy_mods_stats_hc['name'],
+            'ogspy_mod_rank_values' => $ogspy_mods_stats_hc['values']
         );
 
         $this->load->view('welcome_message', $data);
 	}
 
-    private function convert_to_highcharts($data_to_convert){
+    private function convert_to_highcharts_pie($data_to_convert){
         $temp = "";
         foreach($data_to_convert as $key => $value){
 
@@ -59,4 +66,21 @@ class Welcome extends CI_Controller {
         $hc_table = implode(" , ", $temp);
         return($hc_table);
     }
+
+    private function convert_to_highcharts_bar($data_to_convert){
+
+        foreach($data_to_convert as $key => $value){
+
+            $value_name_array [] = "'" .$key. "'";
+            $value_list_array []= $value;
+        }
+        $value_name ="[".implode(" , ", $value_name_array)."]";
+        $value_list ="[".implode(" , ", $value_list_array)."]";
+
+        /*$value_name = "['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']";
+        $value_list = "[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]";*/
+
+        return array('name'=> $value_name ,'values' => $value_list);
+    }
 }
+
--- application/models/Ogspy_mods_stats_model.php
+++ application/models/Ogspy_mods_stats_model.php
@@ -16,12 +16,30 @@ class Ogspy_mods_stats_model extends CI_Model {
         parent::__construct();
     }
 
+    public function get_mod_rank(){
+
+        log_message('debug','Enter in function get_mod_rank');
+
+        $this->db->select('modules.name, COUNT(*)nombre');
+        $this->db->from('ogspy_has_modules');
+        $this->db->join('modules', 'ogspy_has_modules.module_id = modules.id');
+        $this->db->group_by('module_id');
+        $this->db->order_by('nombre', 'DESC');
+        $query = $this->db->get();
+
+        foreach ($query->result() as $row) {
+            $mod_vresult[$row->name] = $row->nombre;
+        }
+        return $mod_vresult;
+    }
+
+
     public function update_mod_data($ogspy_key, $mod_list)
     {
         log_message('debug','Enter in function update_mod_data');
         $ogspy_id = $this->ogspystats_model->get_ogspy_id($ogspy_key);
         log_message('debug','OGSpy_id='.$ogspy_id);
-        //Début transaction
+        //DÃ©but transaction
         $this->clean_ogspy_mods($ogspy_id);
 
         foreach($mod_list as $mod){
@@ -37,7 +55,7 @@ class Ogspy_mods_stats_model extends CI_Model {
     }
 
     private function insert_mod($ogspy_id,$mod){
-        //Vérification que le Mod existe dans la base
+        //VÃ©rification que le Mod existe dans la base
         $mod_id = $this->get_mod_id($mod);
 
        if( -1 == $mod_id){
