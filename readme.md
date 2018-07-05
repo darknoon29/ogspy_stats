ab59d643e70b316af571a87630da94cdd3aa7d92
Darknoon <darknoon@darkcity.fr>
2018-07-05 13:42:46 +0200

    Mise à jour ReadMe
    Dernier script SQL

---
+++ readme.md
@@ -0,0 +1,54 @@
+OGSpy Statistics
+============
+[![GitHub Issues](https://img.shields.io/github/issues/darknoon29/ogspy_stats.svg)](https://github.com/darknoon29/ogspy_stats/issues)
+
+Site Web basé sur Codeigniter pour afficher les statistiques d'OGSpy et de ses modules.
+
+Visitez notre forum [OGSteam.fr](https://forum.ogsteam.fr) pour en savoir plus.
+
+---
+
+### Fonctionalités
+- Statistiques de versions des serveurs OGSpy en activité
+- Statistiques d'utilisation des mods
+
+---
+
+### Installation et Usages
+Fichiers à déposer sur un serveur PHP.
+
+3 tables seront à créer via le script ostats_ogspy.sql contenu dans le dossier sql
+
+Et la configuration de la BDD à faire dans le dossier application/config/database.php
+
+### Site
+
+Le site est disponible à cette addresse : http://darkcity.fr/statistiques/
+
+---
+
+## Contribuer au projet
+
+- Vous pouvez nous aider sur le développement
+- Nous avons besoins de vos idées pour améliorer l'outil
+- Des volontaires pour la documentations dans toutes les langues connues
+- Traductions
+
+Vous pouvez nous poser toutes les questions nécessaires sur notre forum.
+
+## Auteur
+
+[DarkNoon](https://github.com/darknoon29)
+
+### Branches de travail
+
+Nous suivons le traditionnel GitFlow :
+
+* master - Branche Principale, utilisée uniquement pour publier les versions finales de nos outils.
+* development - branche qui contient les développements pour les futures versions
+* release-3.X.Y - Branche contenant les correctifs pour une future version
+
+---
+## License
+
+Le projet est diffusé sous la license **GPLV2**.
--- sql/ostats_ogspy.sql
+++ sql/ostats_ogspy.sql
@@ -3,11 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 01 Août 2015 à 16:48
+-- Généré le :  Dim 13 Septembre 2015 à 17:52
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,6 +19,30 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 --
 -- Base de données :  `ogs_stats`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ostats_modules`
+--
+
+CREATE TABLE IF NOT EXISTS `ostats_modules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `version` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `ostats_modules`
+--
+
+INSERT INTO `ostats_modules` (`id`, `name`, `version`) VALUES
+(1, 'xtense', NULL),
+(2, 'autoupdate', NULL),
+(3, 'attaques', NULL),
+(4, 'cdr', NULL),
+(5, 'superapix', NULL);
 
 -- --------------------------------------------------------
 
@@ -36,8 +61,24 @@ CREATE TABLE IF NOT EXISTS `ostats_ogspy` (
   `php_version` varchar(10) NOT NULL,
   `uni` varchar(5) NOT NULL,
   `pays` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ostats_ogspy_has_modules`
+--
+
+CREATE TABLE IF NOT EXISTS `ostats_ogspy_has_modules` (
+  `ogspy_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  PRIMARY KEY (`ogspy_id`,`module_id`),
+  KEY `fk_ogspy_has_modules_modules1_idx` (`module_id`),
+  KEY `fk_ogspy_has_modules_ogspy_idx` (`ogspy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
